33d07c4f693ea7a9ddc17ef0a3e3daa22a95635b
sam andreatta <samsam.andreatta@gmail.com>
2021-02-24 03:06:37 -0300

    calculate winner

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -29,8 +29,9 @@ export class BoardComponent implements OnInit {
     if(!this.squares[idx]){
       this.squares?.splice(idx, 1, this.player);
       this.xIsNext = !this.xIsNext;
-
     }
+
+    this.winner = this.calculateWinner();
   }
 
   calculateWinner() {
